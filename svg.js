e80f6ac245a0c1bc195a8eb6a8bf0670bb45af10
Drago Tomasic <drag0540@stud.kea.dk>
2022-04-17 14:09:56 +0200

    svg change

--- svg.js
+++ svg.js
@@ -1,4 +1,22 @@
-/*let tl = gsap.timeline({ delay: 0.5 });
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#cloud1", {
+  scrollTrigger: {
+    trigger: "#cloud1",
+    start: "top center",
+    toggleActions: "restart none none none",
+    markers: true,
+  },
+  y: 80,
+  duration: 1,
+});
+
+/*
+gsap.to("#cloud2", { x: 1000 }, 0);
+gsap.to("#cloud3", { x: -1000 }, 0);
+gsap.to("#cloud4", { x: -700, y: 25 }, 0);
+
+let tl = gsap.timeline({ delay: 0.5 });
 tl.from("nav", { duration: 0.6, ease: "power2.out", y: -80 });
 tl.from(".toggle-container", { opacity: 0, duration: 0.6, ease: "power2.out" });
 tl.from(".ulLinks", { opacity: 0, duration: 0.6, ease: "power2.out" });
@@ -11,14 +29,9 @@ tl.from(".profilepic", {
   duration: 0.6,
   x: 60,
   ease: "power2.out",
-});*/
-
-gsap.to(".sattelite", {
-  opacity: 0,
-  duration: 1,
-  ease: "power2.out",
-  delay: 5,
 });
+
+
 gsap.to("#middle", {
   opacity: 0,
   duration: 1,
@@ -38,4 +51,4 @@ gsap.to(".n", {
   duration: 1,
   ease: "power2.out",
   delay: 8,
-});
+});*/
